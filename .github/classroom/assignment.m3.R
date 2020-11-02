eb4895868730e85f570f029768d41177265cdc4d
Your Name <you@example.com>
2020-11-02 10:08:30 +0000

    assignment mod.3

---
+++ .github/classroom/assignment.m3.R
@@ -0,0 +1,17 @@
+data_list <-list(name = c("Buffalo Bill", "Calamity Jane", "Jesse James", "Annie Oakley", "Wyatt Earp"),
+                 sex = factor(c("male", "female", "male", "female", "male")),
+                 obs = list(
+                   moustache = c(TRUE, FALSE, FALSE, FALSE, FALSE),
+                   criminal = c(FALSE, FALSE, TRUE, FALSE, FALSE)
+                 ))
+length(data_list)
+
+# this won't work, fix the problem (use FALSE)
+
+data.frame <- as.data.frame(data_list)
+data_list
+data_list$obs <- NULL
+data_list
+
+# now remove the `obs` from `data_list` before running the next command
+new_data <- as.data.frame(data_list)
